33cba178daedbdd082091a8f16d7dc6cfa9b416e
fabriciosprj <130469931+fabriciosprj@users.noreply.github.com>
2024-08-19 13:14:31 -0300

    Update quasar.conf.js

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -127,8 +127,8 @@ module.exports = function (ctx) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {maximumFileSizeToCacheInBytes: 5000000,}, // only for GenerateSW
       manifest: {
-        name: 'IZING',
-        short_name: 'IZING',
+        name: 'GlobalMensseger',
+        short_name: 'GlobalMensseger',
         description: 'Bot Multi-atendimento para whatsapp',
         display: 'standalone',
         orientation: 'portrait',
